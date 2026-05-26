845a1d9e26a9509922f52f49a903f165634a90ff
Jsala <juliosalag@gmail.com>
2026-05-26 13:26:30 +0200

    Delete .d

--- darkmoon/obj/resources/resource_shader.d
+++
@@ -1,8 +0,0 @@
-obj/resources/resource_shader.o: src/resources/resource_shader.cpp \
- include/darkmoon/resources/resource_shader.hpp \
- libs/glad/include/glad/glad.h libs/glad/include/KHR/khrplatform.h \
- include/darkmoon/resources/resource.hpp
-include/darkmoon/resources/resource_shader.hpp:
-libs/glad/include/glad/glad.h:
-libs/glad/include/KHR/khrplatform.h:
-include/darkmoon/resources/resource.hpp:
